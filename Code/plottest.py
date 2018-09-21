ad78a059173cb51f835ac802e3949c852ff7a7c2
mikaelra <mikaelravndal@gmail.com>
2018-09-21 16:06:54 +0200

    utilites.py is without tests
    utilities.py is without tests and plottest works on read data

--- Code/plottest.py
+++ Code/plottest.py
@@ -4,7 +4,9 @@ from imageio import imread
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+from utilities import *
 
+"""
 # Load the terrain
 terrain1 = imread('data_files/SRTM_data_Norway_1.tif')
 # Show the terrain
@@ -14,3 +16,9 @@ plt.imshow(terrain1, cmap='gray')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.show()
+"""
+
+m, n = 100, 100
+x, y = tifread(mlimit=m, nlimit=n)
+lbeta = LassoReg(x, y, 5, 0.1)
+plot_function(5, lbeta, m, n)
